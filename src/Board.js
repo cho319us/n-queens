4016eb477d1e0344fd8a5b6d1072c6d3037b098a
cho319 <cho319us@gmail.com>
2020-07-10 21:43:58 -0700

    Implemented 8 helper functions in Board class
    - hasRowConflictAt
    - hasAnyRowConflicts
    - hasColConflictAt
    - hasAnyColConflicts
    - hasMajorDiagonalConflictAt
    - hasAnyMajorDiagonalConflicts
    - hasMinorDiagonalConflictAt
    - hasAnyMinorDiagonalConflict

--- src/Board.js
+++ src/Board.js
@@ -62,7 +62,7 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
@@ -79,27 +79,83 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      // get the row
+      var row = this.get(rowIndex);
+      // declare a variable named count to count the number of queen/rock
+      var count = 0;
+      // iterate over the row
+      for (var i = 0; i < row.length; i++) {
+        // check if there is a rock/queen in the current index
+        if (row[i] === 1) {
+          // count add 1
+          count++;
+        }
+      }
+      // if count has more than 1
+      if (count > 1) {
+        return true;
+      } else {
+        return false;
+      }
     },
 
     // test if any rows on this board contain conflicts
+    // hasAnyRowConflicts: function() {
+    //   return false; // fixme
+    // },
+
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      // get the board
+      var board = this.rows();
+      // iterate over the board
+      for (var i = 0; i < board.length; i++) {
+        // if the current row of the board has rows conflict
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
-
-
 
     // COLUMNS - run from top to bottom
     // --------------------------------------------------------------
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      // get the board
+      var board = this.rows();
+      // declare a variable named count to count the number of queen/rock
+      var count = 0;
+      // iterate over the board
+      for (var i = 0; i < board.length; i++) {
+        // get the current row
+        var row = board[i];
+        // if the colIndex at current row has rock/queen
+        if (row[colIndex] === 1) {
+          // count add 1
+          count++;
+        }
+      }
+      // if count has more than 1
+      if (count > 1) {
+        return true;
+      } else {
+        return false;
+      }
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      // get the board
+      var board = this.rows();
+      // iterate over the board.length/column length/row length
+      for (var i = 0; i < board.length; i++) {
+        // if the current index has column conflict
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -109,12 +165,42 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      // get the board
+      var board = this.rows();
+      // declare count
+      var count = 0;
+      // iterate over the board
+      for (var i = 0; i < board.length; i++) {
+        // get the row
+        var row = board[i];
+        // if the input index at current row has queen/rock
+        if (row[majorDiagonalColumnIndexAtFirstRow] === 1) {
+          // count add 1
+          count++;
+        }
+        // input index add 1
+        majorDiagonalColumnIndexAtFirstRow++;
+      }
+      // if count has more than 1
+      if (count > 1) {
+        return true;
+      } else {
+        return false;
+      }
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      // get the board
+      var board = this.rows();
+      // iterate over the board from negative number to handle the left bottom part
+      for (var i = 1 - board.length; i < board.length; i++) {
+        // if current index has major diagonal conflict
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -124,12 +210,42 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      // get the board
+      var board = this.rows();
+      // declare count
+      var count = 0;
+      // iterate over the board
+      for (var i = 0; i < board.length; i++) {
+        // get the row
+        var row = board[i];
+        // if the input index at current row has queen/rock
+        if (row[minorDiagonalColumnIndexAtFirstRow] === 1) {
+          // count add 1
+          count++;
+        }
+        // input index subtract 1
+        minorDiagonalColumnIndexAtFirstRow--;
+      }
+      // if count has more than 1
+      if (count > 1) {
+        return true;
+      } else {
+        return false;
+      }
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      // get the board
+      var board = this.rows();
+      // iterate over the board from negative number to handle the left bottom part
+      for (var i = board.length + board.length - 1; i > 0; i--) {
+        // if current index has minor diagonal conflict
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
